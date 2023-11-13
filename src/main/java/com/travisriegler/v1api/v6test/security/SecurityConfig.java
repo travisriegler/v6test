3de190dc2bda6e6ca4cb34e16f236f8588cf922c
Travis Riegler <travisriegler@protonmail.com>
2023-11-13 12:01:38 -0500

    opened cors

--- src/main/java/com/travisriegler/v1api/v6test/security/SecurityConfig.java
+++ src/main/java/com/travisriegler/v1api/v6test/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -12,6 +13,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(c -> c.disable())
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowCredentials(true); // You might not need this line
+                            config.addAllowedOriginPattern("*");
+                            config.addAllowedMethod("*");
+                            config.addAllowedHeader("*");
+                            return config;
+                        })
+                )
                 .headers(h -> h
                         .contentSecurityPolicy(p -> p
                                 .policyDirectives("frame-ancestors 'self'")

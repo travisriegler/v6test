513f53608d8999d4df067013e41f765d054e02aa
Travis Riegler <travisriegler@protonmail.com>
2023-11-13 12:23:43 -0500

    added uuid for healthcheck

--- src/main/java/com/travisriegler/v1api/v6test/controller/HealthCheckController.java
+++ src/main/java/com/travisriegler/v1api/v6test/controller/HealthCheckController.java
@@ -3,23 +3,17 @@ package com.travisriegler.v1api.v6test.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @RestController
 public class HealthCheckController {
 
+    private final String instanceUuid = UUID.randomUUID().toString();
+
     @GetMapping("/healthcheck")
     public ResponseEntity<String> checkHealth() {
-        //testing github
-        RestTemplate restTemplate = new RestTemplate();
-        String instanceId;
-        try {
-            instanceId = restTemplate.getForObject("http://169.254.169.254/latest/meta-data/instance-id", String.class);
-        } catch (Exception e) {
-            instanceId = "Unknown";  // or handle exception in some other way
-        }
-        return ResponseEntity.ok("healthy, instance-id: " + instanceId);
+        return ResponseEntity.ok("healthy, instance-uuid: " + instanceUuid);
     }
-
 }
 
